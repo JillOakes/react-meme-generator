f8e7e1f5d45b4b71f572344bcba842487cd6f047
Jill Oakes <jill.oakes@beckshybrids.com>
2022-07-19 15:42:53 -0400

    Update to vid 3.20 mvp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import logo from './logo.svg';
 // import './App.css';
 import Header from "./components/Header";
+import Meme from "./components/Meme";
 
 function App() {
   return (
     <div className="App">
       <div>
         <Header />
+        <Meme />
       </div>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
