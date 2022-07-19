aeaf1566e5c1cda0f762c2512785af66ba55e0ab
Jill Oakes <jill.oakes@beckshybrids.com>
2022-07-19 11:09:35 -0400

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import logo from "../images/picard.png";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img src={logo} className="header--image" alt="exasperated Picard"/>
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
